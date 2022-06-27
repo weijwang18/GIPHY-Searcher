b18d79d68fd92de6ba9d0b08a90b0734b4d6cfdb
weijwang18@gmail.com <weijwang18@gmail.com>
2022-06-27 15:39:56 -0700

    add functionality so users can see trending, random content from Giphy
    Co-authored-by: DonovanWeber <donovanweber03@gmail.com>

--- src/index.js
+++ src/index.js
@@ -10,12 +10,11 @@ $(document).ready(function () {
     $("#search").val();
 
     let request = new XMLHttpRequest();
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${userSearch}&limit=1&offset=0&rating=g&lang=en`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${userSearch}&limit=3&offset=0&rating=g&lang=en`;
 
     request.onreadystatechange = function () {
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
-        console.log(response);
         getElements(response);
       }
     };
@@ -23,9 +22,67 @@ $(document).ready(function () {
     request.open("GET", url, true);
     request.send();
 
-       function getElements(response) {
+    function getElements(response) {
+      $("#output-image1").html(
+        `<img src="${response.data[0].images.original.url}"></img>`
+      );
+      $("#output-image2").html(
+        `<img src="${response.data[1].images.original.url}"></img>`
+      );
+      $("#output-image3").html(
+        `<img src="${response.data[2].images.original.url}"></img>`
+      );
+    }
+  });
+
+  $("button#trending").click(function (event) {
+    event.preventDefault();
+
+    let request = new XMLHttpRequest();
+    const url = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=3&rating=g`;
+
+    request.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        outputTrending(response);
+      }
+    };
+    request.open("GET", url, true);
+    request.send();
+
+    function outputTrending(response) {
+      $("#output-image1").html(
+        `<img src="${response.data[0].images.original.url}"></img>`
+      );
+      $("#output-image2").html(
+        `<img src="${response.data[1].images.original.url}"></img>`
+      );
+      $("#output-image3").html(
+        `<img src="${response.data[2].images.original.url}"></img>`
+      );
+    }
+  });
+  $("button#random").click(function (event) {
+    event.preventDefault();
+
+    let request = new XMLHttpRequest();
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&tag=&rating=g`;
+
+    request.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
         console.log(response);
-      $("#output-image").html(`<img src="${response.data[0].images.original.url}"></img>`);
-  }
-});
+        outputRandom(response);
+      }
+    };
+    request.open("GET", url, true);
+    request.send();
+
+    function outputRandom(response) {
+      $("#output-image1").html(
+        `<img src="${response.data.images.original.url}"></img>`);
+      $("#output-image2").empty();
+      $("#output-image3").empty();
+    }
+  });
 });
